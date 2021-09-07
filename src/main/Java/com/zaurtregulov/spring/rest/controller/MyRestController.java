721e5aadd606322e288a9c60fb96b51502ceb573
sartvas <47525820+sartvas@users.noreply.github.com>
2021-09-07 17:47:34 +0300

    New RESTApi annotation @PathVariable. Get employee from id

--- src/main/Java/com/zaurtregulov/spring/rest/controller/MyRestController.java
+++ src/main/Java/com/zaurtregulov/spring/rest/controller/MyRestController.java
@@ -7,6 +7,7 @@ import com.zaurtregulov.spring.rest.service.EmployeeService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -21,5 +22,11 @@ public class MyRestController {
         List<Employee> allEmployees = employeeService.getAllEmployees();
         return allEmployees;
     } 
+    
+    @GetMapping("/employees/{id}")
+   public Employee getEmployee(@PathVariable int id){
+       Employee employee = employeeService.getEmployee(id);
+       return employee;
+   }
     
 }
